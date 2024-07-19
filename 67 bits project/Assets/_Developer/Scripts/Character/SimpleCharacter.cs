a3c6d9bac4391e6682275ede32d7361b66c39077
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-19 10:20:25 -0300

    do lerp to move character to player stack pivot

--- 67 bits project/Assets/_Developer/Scripts/Character/SimpleCharacter.cs
+++ 67 bits project/Assets/_Developer/Scripts/Character/SimpleCharacter.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Test.Characters
 {
     public class SimpleCharacter : MonoBehaviour
     {
-        public void Punch()
+        [SerializeField] private Transform _characterPivot;
+        [SerializeField] private SimpleCharacterData _data;
+
+        private float _timeToMove;
+        private float _elapsedTime;
+        private bool _hasChangedPosition;
+
+        public Transform Pivot { get { return _characterPivot; } }
+        public bool HasChangedPosition { get { return _hasChangedPosition; } }
+
+        public void Start()
         {
-            Debug.Log("has been punched");
+            Init();
+        }
+
+        public void Init()
+        {
+            _elapsedTime = 0f;
+            _hasChangedPosition = false;
+        }
+
+        public void ChangePosition(Transform destination)
+        {
+            Debug.Log("Change Position");
+            _hasChangedPosition = true;
+            transform.SetParent(destination, false);
+            StartCoroutine(ChangePositionCoroutine(destination.localPosition));
+        }
+
+        public IEnumerator ChangePositionCoroutine(Vector3 destination)
+        {
+            while(_elapsedTime < _data.timeToMove)
+            {
+                _elapsedTime += Time.deltaTime;
+                transform.localPosition = Vector3.Lerp(_characterPivot.localPosition, destination, _elapsedTime / _data.timeToMove);
+            }
+               
+            transform.localPosition = Vector3.zero;
+            yield return null;
         }
     }
 }
