d0a6493588cf3354f7f3718aee179219c1b50ebd
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-19 10:17:23 -0300

    player data changes

---
+++ 67 bits project/Assets/_Developer/Scripts/SO/PlayerData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Test.Characters
+{
+    [CreateAssetMenu(menuName = "Data/Player")]
+    public class PlayerData : ScriptableObject
+    {
+        public string characterName;
+        public float maxSpeed;
+        public float acceleration;
+        public float deaceleration;
+        public float rotationSmoothTime;
+    }
+}
