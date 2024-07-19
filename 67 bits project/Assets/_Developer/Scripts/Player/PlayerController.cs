ece7f4d4be5122cb7acdac1ca083efef1900691d
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-19 10:20:03 -0300

    added player pivot reference

--- 67 bits project/Assets/_Developer/Scripts/Player/PlayerController.cs
+++ 67 bits project/Assets/_Developer/Scripts/Player/PlayerController.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace Test.Characters
 {
     public class PlayerController : PlayerCharacter
     {
-       
+        [SerializeField] private Transform _characterStackParent;
+        public Transform StackPivot { get { return _characterStackParent; } } 
     }
 }
