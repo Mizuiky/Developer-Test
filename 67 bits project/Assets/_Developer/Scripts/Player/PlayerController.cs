6a1c6241c019f3567f85c23f08864033a59c9ffe
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-19 12:39:28 -0300

    change stack position pivot based on last character in the stack

--- 67 bits project/Assets/_Developer/Scripts/Player/PlayerController.cs
+++ 67 bits project/Assets/_Developer/Scripts/Player/PlayerController.cs
@@ -4,7 +4,32 @@ namespace Test.Characters
 {
     public class PlayerController : PlayerCharacter
     {
-        [SerializeField] private Transform _characterStackParent;
-        public Transform StackPivot { get { return _characterStackParent; } } 
+        [SerializeField] private Transform _stackParent;
+     
+        private Transform _stackPosition;
+        private Transform _newPivot;
+        private int _stackCount = 0;
+        private float _stackHeight;
+        public Transform StackPosition { get { return _newPivot; } }     
+        public Transform StackParent { get { return _stackParent; } }
+    
+        public override void Init()
+        {
+            _stackCount = 0;
+            _stackPosition = _stackParent;
+            _newPivot = _stackPosition;
+            base.Init();
+        }
+
+        public void SetStackPosition()
+        {
+            if(_stackCount == 0)
+                _stackHeight = _stackParent.localPosition.y;
+            else
+                _stackHeight = _stackParent.localPosition.y * _stackCount;
+
+            _newPivot.position = new Vector3(_stackParent.position.x, _stackHeight, _stackParent.position.z);
+            _stackCount++;
+        }
     }
 }
