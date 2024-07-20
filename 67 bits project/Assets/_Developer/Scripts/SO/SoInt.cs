4ce34fc77132fd456604c99999f4e45b7742a219
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-20 11:28:36 -0300

    created scriptable objects to be flexible to update money and character values

---
+++ 67 bits project/Assets/_Developer/Scripts/SO/SoInt.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test.SOValue
+{
+    [CreateAssetMenu(menuName = "Data/SoInt")]
+    public class SoInt : ScriptableObject
+    {
+        public int value;
+    }
+}
