6d6639412480f95906c87693f74e2d03173312b0
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-21 19:00:00 -0300

    created color item prefabs to be added to the shop

---
+++ 67 bits project/Assets/_Developer/Scripts/UI/Shop/ColorItem.cs
@@ -0,0 +1,42 @@
+using Test.Event;
+using UnityEngine;
+
+namespace Test.Shop
+{
+    public class ColorItem : ShopItemBase
+    {
+        [SerializeField] private ColorItemData _colorItemData;
+        [SerializeField] private GameEventObject _onColorUse;
+
+        public ColorItemData ColorData { get { return _colorItemData; } }
+
+        public override void Init()
+        {
+            _price = _colorItemData.price;
+            _name = _colorItemData.name;
+            base.Init();      
+        }
+
+        public override void SellItem()
+        {
+            base.SellItem();
+            _soldOut = "Use";
+        }   
+        
+        public override void CheckStoreItem()
+        {
+            if (_hasSold)
+            {
+                UseItem();
+                return;
+            }
+            base.CheckStoreItem();
+        }
+
+        public override void UseItem()
+        {
+            base.UseItem();
+            _onColorUse.Invoke(_colorItemData);
+        }
+    }
+}
