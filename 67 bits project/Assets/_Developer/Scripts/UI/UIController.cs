63bb29f568482f40f173073c9f5a63c215a9f6b8
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-21 19:06:21 -0300

    created shop call backs at UIController

--- 67 bits project/Assets/_Developer/Scripts/UI/UIController.cs
+++ 67 bits project/Assets/_Developer/Scripts/UI/UIController.cs
@@ -1,9 +1,45 @@
+using Test.Event;
 using UnityEngine;
+using Test.Shop;
 
 namespace Test.UI
 {
     public class UIController : MonoBehaviour
     {
         [SerializeField] private HUD _hud;
+        [SerializeField] private ShopController _shopController;
+        [SerializeField] private GameEventObject _onItemSold;
+
+        public void OnEnable()
+        {
+            _onItemSold.Subscribe(ShopItemCallBack);
+        }
+
+        public void Start()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            _shopController.Init();
+        }
+
+        #region Shop
+        private void ShopItemCallBack(object args)
+        {
+            _shopController.Sell((ShopItemBase)args);
+        }
+
+        public void OpenShop(bool canOpen)
+        {
+            _shopController.Open(canOpen);   
+        }
+        #endregion
+
+        public void OnDisable()
+        {
+            _onItemSold.Unsubscribe(ShopItemCallBack);
+        }
     }
 }
