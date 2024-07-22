8dc8efd047312c7462bbbd082dc882b846e3e288
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-07-22 19:37:43 -0300

    instead using update to calculate fps, created coroutine with interval value to be updated

--- 67 bits project/Assets/_Developer/Scripts/Managers/FPSManager.cs
+++ 67 bits project/Assets/_Developer/Scripts/Managers/FPSManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using Test.SOValue;
 using TMPro;
+using System.Collections;
+using Unity.IntegerTime;
 
 namespace Test.Manager
 {
@@ -9,33 +11,51 @@ namespace Test.Manager
         [SerializeField] private TextMeshProUGUI _fpsText;
         [SerializeField] private Color _highFpsColor;
         [SerializeField] private Color _lowFpsColor;
+        [SerializeField] private float _intervalValue;
 
         private float _fpsValue;
+        private float _frameCount;
         private string _fpsFormatedText;
-        private float _deltaTime;
+        private float _elapsedTime;
+        private Color _currentFpsColor;
 
         public void Init()
         {
             Application.targetFrameRate = 60;
+            StartCoroutine(FpsCoroutine());
         }
 
         public void Update()
         {
-            CalculateFPSValue();
+            _frameCount++;
+            _elapsedTime += Time.deltaTime;
+        }
+        private IEnumerator FpsCoroutine()
+        {
+            //game is running
+            while(true)
+            {
+                yield return new WaitForSeconds(_intervalValue);
+                CalculateFPSValue();
+               
+            }
         }
 
         private void CalculateFPSValue()
         {
-            _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
-            _fpsValue = 1.0f / _deltaTime;
-            _fpsFormatedText = string.Format("FPS: {0:0.}", _fpsValue);
+            _fpsValue = _frameCount / _elapsedTime;
+            _fpsFormatedText = string.Format("FPS: {0.0}", _fpsValue);
 
-            if(_fpsValue >= 30f)
-                _fpsText.color = _highFpsColor;
-            else
-                _fpsText.color = _lowFpsColor;
+            _currentFpsColor = _fpsValue >= 30f ? _highFpsColor : _lowFpsColor;
 
-            _fpsText.text = _fpsFormatedText;
+            if (_fpsText.color != _currentFpsColor)
+            {
+                _fpsText.text = _fpsFormatedText;
+                _fpsText.color = _currentFpsColor;
+            }
+
+            _frameCount = 0;
+            _elapsedTime = 0;
         }
     }
 }
